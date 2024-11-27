ce2875c60a526eb9ae435d65222d3b0be21f3623
AriefMade <ariepmade@gmail.com>
2024-11-28 00:05:09 +0800

    berandapkl update

--- lib/Pekerja Keliling/Beranda/berandaPKL.dart
+++ lib/Pekerja Keliling/Beranda/berandaPKL.dart
@@ -3,11 +3,13 @@ import 'package:digicoop/Pekerja Keliling/nasabah/nasabahPKL.dart';
 import 'package:digicoop/Pekerja Keliling/profil/profilPKL.dart';
 
 class BerandaPkl extends StatefulWidget {
-  const BerandaPkl ({Key? key}) : super(key: key);
-@override
-State<BerandaPkl> createState() => _BerandaPklState();}
+  const BerandaPkl({Key? key}) : super(key: key);
 
-class _BerandaPklState extends State<BerandaPkl>{
+  @override
+  State<BerandaPkl> createState() => _BerandaPklState();
+}
+
+class _BerandaPklState extends State<BerandaPkl> {
   int _selectedIndex = 0;
 
   @override
@@ -20,6 +22,7 @@ class _BerandaPklState extends State<BerandaPkl>{
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: 30),
               // Header
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -28,9 +31,9 @@ class _BerandaPklState extends State<BerandaPkl>{
                     children: [
                       CircleAvatar(
                         backgroundImage: AssetImage('assets/profile.jpg'), // Sesuaikan gambar
-                        radius: 30,
+                        radius: 27,
                       ),
-                      SizedBox(width: 10),
+                      SizedBox(width: 20),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -44,21 +47,29 @@ class _BerandaPklState extends State<BerandaPkl>{
                       ),
                     ],
                   ),
-                  Stack(
-                    children: [
-                      Icon(Icons.notifications, size: 28),
-                      Positioned(
-                        right: 0,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.red,
-                          radius: 8,
-                          child: Text(
-                            '5',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xB2CFB4A4)
+                    ),
+                    padding: EdgeInsets.all(6),
+                    child: Stack(
+                      children: [
+                        Icon(Icons.notifications, size: 28),
+                        Positioned(
+                          right: 0,
+                          // Adjusted position for notification icon
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            radius: 8,
+                            child: Text(
+                              '5',
+                              style: TextStyle(color: Colors.white, fontSize: 12),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -66,36 +77,143 @@ class _BerandaPklState extends State<BerandaPkl>{
 
               // Ringkasan Kunjungan
               Card(
+                color: Color(0xFF69574D),
                 elevation: 3,
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     children: [
-                      Text(
-                        'Rabu, 15 Desember 2024',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                      Row( // Start a Row to add an icon next to the text
+                        mainAxisAlignment: MainAxisAlignment.start, // Align children to the start (left)
+                        children: [
+                          Icon(
+                            Icons.calendar_today, // Add the calendar icon
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 8), // Add some space between the icon and the text
+                          Text(
+                            'Rabu, 15 Desember 2024',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ],
                       ),
                       SizedBox(height: 10),
+
+                      //25 customer
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Column(
-                            children: [
-                              Text('25',
+                          Container(
+                            padding: const EdgeInsets.all(13.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  '25',
                                   style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold)),
-                              Text('Customer Visited'),
-                            ],
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF69574D),
+                                  ),
+                                ),
+                                Text(
+                                  'Customer Visited',
+                                  style: TextStyle(
+                                    color: Color(0xFF67554B),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                          Column(
-                            children: [
-                              Text('11',
+                          //11 customer remaining
+                          Container (
+                            padding: const EdgeInsets.all(13.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child : Column(
+                              children: [
+                                Text(
+                                  '11',
                                   style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold)),
-                              Text('Customer Remaining'),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF69574D),
+                                  ),
+                                ),
+                                Text('Customer Remaining',
+                                  style : TextStyle(
+                                    color: Color(0xFF69574D),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                          //beda bagian
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+
+              // Total Dana and Navigasi Aktivitas combined
+              Card(
+                color: Color(0xFF69574D),
+                elevation: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Total Dana
+                      Text(
+                        'Total Dana',
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      SizedBox(height: 2),
+                      Text(
+                        'Rp2.000.000,00 /hari ini',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Aktivitas',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          // Navigasi Aktivitas
+                          GridView.count(
+                            crossAxisCount: 2,
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            crossAxisSpacing: 0, // Remove spacing between columns
+                            mainAxisSpacing: 0, // Remove spacing between rows
+                            childAspectRatio: 2.7, // Keep the child aspect ratio to make cards wider
+                            children: [
+                              _buildMenuCard('Simpanan', Icons.arrow_upward),
+                              _buildMenuCard('Penarikan', Icons.arrow_downward),
+                              _buildMenuCard('Rekap Harian', Icons.calendar_today),
+                              _buildMenuCard('Riwayat Nasabah', Icons.person),
                             ],
                           ),
                         ],
@@ -105,49 +223,11 @@ class _BerandaPklState extends State<BerandaPkl>{
                 ),
               ),
               SizedBox(height: 20),
-
-              // Informasi Total Dana
-              Card(
-                elevation: 3,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Total Dana',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      SizedBox(height: 5),
-                      Text(
-                        'Rp2.000.000,00 /hari ini',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Navigasi Aktivitas
-              GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                children: [
-                  _buildMenuCard('Simpanan', Icons.arrow_upward),
-                  _buildMenuCard('Penarikan', Icons.arrow_downward),
-                  _buildMenuCard('Rekap Harian', Icons.calendar_today),
-                  _buildMenuCard('Riwayat Nasabah', Icons.person),
-                ],
-              ),
-              SizedBox(height: 20),
-
               // Transaksi Terbaru
-              Text('Transaksi Terbaru',
-                  style:
-                  TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Text(
+                'Transaksi Terbaru',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
               SizedBox(height: 10),
               _buildTransactionItem('Simpanan', 'Rp100.000', '10.30'),
               _buildTransactionItem('Simpanan', 'Rp100.000', '10.30'),
@@ -159,19 +239,23 @@ class _BerandaPklState extends State<BerandaPkl>{
         selectedItemColor: Colors.brown,
         unselectedItemColor: Colors.grey,
         currentIndex: _selectedIndex,
-                  onTap: (index) {
-                    setState((){
-              _selectedIndex = index;
-              });
-          if (index == 1){
-            Navigator.push(context,
-                MaterialPageRoute(builder : (context) => NasabahPkl())); }
-            else if (index == 2){
-              Navigator.push(context,
-                  MaterialPageRoute(builder : (context) => profilPkl()));
-    }
-  },
-  items: [
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NasabahPkl()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => profilPkl()),
+            );
+          }
+        },
+        items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
           BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Nasabah'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
@@ -179,20 +263,29 @@ class _BerandaPklState extends State<BerandaPkl>{
       ),
     );
   }
-  }
+
   Widget _buildMenuCard(String title, IconData icon) {
     return Card(
+      color: Colors.white, // Set color for each menu card
       elevation: 3,
       child: InkWell(
-        onTap: () {}, // Tambahkan navigasi
+        onTap: () {}, // Add navigation if needed
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(icon, size: 30, color: Colors.brown),
-              SizedBox(height: 10),
-              Text(title, textAlign: TextAlign.center),
+              SizedBox(width: 10), // Fixed width compared to height
+              Text(title,
+                  style : TextStyle(
+                    color: Color(0xFF543E2D),
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    height: 0.15,
+                    letterSpacing: -0.32,),
+              ),
             ],
           ),
         ),
@@ -207,3 +300,4 @@ class _BerandaPklState extends State<BerandaPkl>{
       trailing: Text(time),
     );
   }
+}
